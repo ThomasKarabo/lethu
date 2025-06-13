4621291e454ab06ff51ed65c3cafd8693781af1b
Thomas Karabo Mohlapo <79021656+ThomasKarabo@users.noreply.github.com>
2025-06-13 08:11:54 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.header('❤️  Good Morning Mthwaka')
+good_morning = st.button('Click here uzobona')
+if good_morning:
+    st.success('''Good morning! I'm wishing you to have an awesome day filled with happiness and love. Start your day with positivity and gratitude and know that nna motho wa kao ko rata thata. 
+               🎓 This is your final stretch! Remember, all your hard work and dedication have brought you here. Believe in yourself, stay focused, and give it your best. Success is just an exam away. You can do it! Best of luck!''')
+
+if scripture := st.button('Scripture for the day'):
+    st.write("""Today's scripture is: 
+Philippians 4:13 - "I can do all things through Christ who gives me strength."
+            Know that I love you and I appreciate you in my life neh.""")
