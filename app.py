4242ed1f5d5f78d11966c22c09fb0966698440ac
Thomas Karabo Mohlapo <79021656+ThomasKarabo@users.noreply.github.com>
2025-06-13 08:14:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.header('❤️  Good Morning Mthwaka')
 good_morning = st.button('Click here uzobona')
 if good_morning:
     st.success('''Good morning! I'm wishing you to have an awesome day filled with happiness and love. Start your day with positivity and gratitude and know that nna motho wa kao ko rata thata. 
-               🎓 This is your final stretch! Remember, all your hard work and dedication have brought you here. Believe in yourself, stay focused, and give it your best. Success is just an exam away. You can do it! Best of luck!''')
+               🎓 This is your final lap! Believe in yourself, stay focused, and give it your best aker.You can do it! Best of luck!''')
 
 if scripture := st.button('Scripture for the day'):
     st.write("""Today's scripture is: 
